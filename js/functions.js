21295ace214b13b4edabac6b82c95a42b61541a5
komdiv <103315729+komdiv@users.noreply.github.com>
2022-04-10 19:12:47 +0300

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,385 @@
+'use strict';
+
+function fillMapVo(anyMap){
+	np = 0;
+	//anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","","","",1));
+	anyMap.set(np++, new Vo("к_нтинет","о","а","е",""));
+	anyMap.set(np++, new Vo("к_ридор","о","а","э",""));
+	anyMap.set(np++, new Vo("кр_терий","и","е","о",""));
+	anyMap.set(np++, new Vo("л_боратория","а","о","и",""));
+	anyMap.set(np++, new Vo("лаб_ратория","о","а","е",""));
+	anyMap.set(np++, new Vo("л_коничность","а","о","и",""));
+	anyMap.set(np++, new Vo("лак_ничность","о","а","я",""));
+	anyMap.set(np++, new Vo("л_терея","о","а","и",""));
+	anyMap.set(np++, new Vo("лот_рея","е","и","а",""));
+	anyMap.set(np++, new Vo("м_кулатура","а","о","ы",""));
+	anyMap.set(np++, new Vo("макул_тура","а","о","у",""));
+	anyMap.set(np++, new Vo("м_рионетка","а","о","ь",""));
+	anyMap.set(np++, new Vo("мар_онетка","и","е","а",""));
+	anyMap.set(np++, new Vo("мари_нетка","о","а","ы",""));
+	anyMap.set(np++, new Vo("п_ссимизм","е","и","о",""));
+	anyMap.set(np++, new Vo("пи_имизм","сс","с","гг",""));
+	anyMap.set(np++, new Vo("тр_ллейбус","о","а","э",""));
+	anyMap.set(np++, new Vo("тро_ейбус","лл","л","ллл",""));
+	anyMap.set(np++, new Vo("к_ллекция","о","а","и",""));
+	anyMap.set(np++, new Vo("ко_екция","лл","л","ллл",""));
+	anyMap.set(np++, new Vo("су_икс","фф","ффф","ф",""));
+	anyMap.set(np++, new Vo("м_теор","е","и","а",""));
+	anyMap.set(np++, new Vo("мет_ор","е","","о",""));
+	anyMap.set(np++, new Vo("п_лисадник","а","о","и",""));
+	anyMap.set(np++, new Vo("пал_садник","и","а","е",""));
+	anyMap.set(np++, new Vo("п_ликлиника","о","а","и",""));
+	anyMap.set(np++, new Vo("пол_клиника","и","е","а",""));
+	anyMap.set(np++, new Vo("поликли_ика","н","нн","ннн",""));
+	anyMap.set(np++, new Vo("р_пертуар","е","и","л",""));
+	anyMap.set(np++, new Vo("реп_ртуар","е","и","а",""));
+	anyMap.set(np++, new Vo("_диннадцать","о","а","и",""));
+	anyMap.set(np++, new Vo("оди_адцать","нн","н","нннн",""));
+	anyMap.set(np++, new Vo("одинна_цать","д","т","б",""));
+	anyMap.set(np++, new Vo("_тчизна","о","а","и",""));
+	anyMap.set(np++, new Vo("тру_еник","ж","жж","жжж",""));
+	anyMap.set(np++, new Vo("труж_ник","е","и","а",""));
+	anyMap.set(np++, new Vo("тружи_ик","н","нн","нннннн",""));
+	anyMap.set(np++, new Vo("ун_верситет","и","е","а",""));
+	anyMap.set(np++, new Vo("унив_рситет","е","и","а",""));
+	anyMap.set(np++, new Vo("ч_ресчур","е","и","а",""));
+	anyMap.set(np++, new Vo("чер_счур","е","и","а",""));
+	anyMap.set(np++, new Vo("экз_мпляр","е","и","а",""));
+	anyMap.set(np++, new Vo("эксп_диция","е","и","а",""));
+	anyMap.set(np++, new Vo("ю_ый","н","нн","ннн",""));
+
+
+
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","йцу","цйу","йуц",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","цук","цку","уцк",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","уке","куе","еук",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","кен","екн","кне",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","енг","нег","ген",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","нгш","шгн","гшн",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","гшщ","шгщ","шщг",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","шщз","щшз","шзщ",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","щзх","щхз","зщх",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","зхъ","хзъ","хъх",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","фыв","ыфв","выф",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","ыва","выа","авы",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","вап","авп","впа",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","апр","пар","пав",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","про","рпа","пор",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","рол","орп","рлп",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","олд","лод","дро",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","лдж","длж","дло",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","джэ","эжд","ждэ",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","ячс","чяс","сяч",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","чсм","счя","счм",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","сми","мси","исч",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","мит","имс","мти",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","ить","тиь","ьим",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","тьб","ьти","иьб",1));
+	// anyMap.set(np++, new Vo("Найдите верное сочетание на клавиатуре","ьбю","бью","тбю",1));
+	
+	anyMap.set(np++, new Vo(`ТЕСТ ЗАВЕРШЕН! Повторить там, где ошиблись?`,"да!","нет","начать с самого начала",0));
+
+	//Vo.voVsegoInteger = window.maps.size;
+	Vo.voTekInteger = 0;		
+//	window.maps.set(np++, new Vo("в_кзал ","а","о","е",2));
+//	window.maps.set(np++, new Vo("с_лдат","о","а","больше не спрашивать",1));
+//	window.maps.set(np++, new Vo("г_нерал","е","и","больше не спрашивать",1));
+//	window.maps.set(np++, new Vo("п_дистал","пъ","пь","больше не спрашивать",2));
+//	window.maps.set(np++, new Vo("мы_","ш","шь","больше не спрашивать",2));
+//	window.maps.set(np++, new Vo(`ТЕСТ ЗАВЕРШЕН! Повторить там, где ошиблись?`,"да!","нет","начать с самого начала",1));
+
+}
+
+function proverka(intOtvet){
+	//последний вопрос - сразу на финиш
+	//последний заход
+
+	if (Vo.voTekInteger == Vo.voVsegoInteger-1){
+		finish(intOtvet);
+	}else{
+	
+	//if (window.voTekInteger>= window.maps.size){finish(intOtvet);}
+	
+	//первый заход
+	//let dd = window.voTek.vopros;
+	//alert(`sub05 = ${dd}`);
+	if (0==1){
+		
+	//if (VoStart.voVsegoInteger) {}
+// 	if ((window.voTek.vopros).substring(0,6) == "привет") {
+//		if (window.voTek.otvetTrue != intOtvet){
+//			alert("вернись, когда передумаешь");
+//			}else{
+//				//window.voTekInteger=window.voTekInteger+1;
+//				//window.voTek.iknow = 1;
+//				window.voTekInteger++;
+//				//display(window.voTekInteger);
+//				display(Vo);			
+//			}
+	}else{
+	//остальные заходы
+		//if (window.voTek.otvetTrue != intOtvet){
+		let voTek = Vo.getTek();
+		if (Vo.getTek().otvetTrue != intOtvet){	
+			let element = 0;
+			if (intOtvet ==1){element = document.querySelector("#otv1")}
+			if (intOtvet ==2){element = document.querySelector("#otv2")}
+			if (intOtvet ==3){element = document.querySelector("#otv3")}
+			let intTek = document.querySelector("#intTek");
+			let msgErrors = document.querySelector("#msgErrors");
+			//if (window.voTek.iknow == 0) {window.voTek.iknow = -1;};
+			element.textContent = "...";
+			if (Vo.getTek().errored == false){
+				//let newObj = Vo.getTek().getCopyObj();
+				window.mapsError.set(window.mapsErrorInteger++, Vo.getTek());
+				Vo.getTek().errored = true;
+				}
+			intTek.textContent = Vo.getMessageError("XfromN");
+			msgErrors.textContent = Vo.getMessageError("UHaveXErrors");
+			//intTek.textContent = Vo.getMessageError();
+			//alert(`Error ${window.mapsErrorInteger-1}`);
+			//let mcgME = myClass.getMessageError();
+			//alert(`Error ${mcgME}`);
+		}else{
+			Vo.getTek().errored = false;
+			//window.voTekInteger++;
+			Vo.next();
+			//if (window.voTekInteger == window.maps.size){finish(intOtvet);}
+			//display(window.voTekInteger);
+			display(Vo);
+
+			}
+	}
+	}
+}
+
+
+function finish(otvet){
+	if (otvet==1){
+		//alert(`ответ ${otvet}`);
+		if(window.mapsError.size==0){alert(`Замечательно! Пройдены все вопросы!!!`);}
+		window.maps.clear();
+		window.maps = new Map(window.mapsError);
+		//window.maps = window.mapsError;
+		window.mapsError.clear();
+		Vo.voVsegoInteger = window.maps.size;
+		Vo.voTekInteger = 0;
+		//alert(`ошибок = ${window.maps.size}`);
+		window.maps.set(Vo.voVsegoInteger, new Vo(`Материал закреплен! Повторить там, где ошиблись?`,"да!","нет","начать с самого начала",1));
+		window.mapsErrorInteger=0;
+		//Vo.next();
+		//window.voTekInteger = 0;
+		display(Vo);
+	}
+	if (otvet==2) {
+		//alert(`ответ ${otvet}`);
+		if (intOtvet ==2){
+			let element = document.querySelector("#otv2")
+			element.textContent = "...";
+		}
+		//window.mapsError.clear();
+		//window.mapsErrorInteger=0;
+		//display(window.voTekInteger);
+	}
+	if (otvet==3){
+		fillMapVo(window.maps);
+		//Очищу и хранилище ошибок:
+		window.mapsError.clear();
+		window.mapsErrorInteger=0;
+		display(Vo);
+	}
+	//alert(`финиш ${otvet}`);
+}
+
+function creatDiv(myClass){
+	myClass.creatDiv();
+}
+
+function display(myClass){
+	let fvopros = document.querySelector("#vopros");
+	//let fotv1 = document.forms("voForm").getElementById("#otv1");
+	let fotv1 = document.querySelector("#otv1");
+	let fotv2 = document.querySelector("#otv2");
+	let fotv3 = document.querySelector("#otv3");
+	let intTek = document.querySelector("#intTek");
+	let msgErrors = document.querySelector("#msgErrors");
+	//window.voTek = window.maps.get(int);
+	//let objVo = window.voTek;
+	
+	let objVo = myClass.getTek();
+	//document.getElementById("demo").style.fontSize = "x-large";
+	fvopros.style.fontSize = "xx-large";
+	fvopros.textContent = objVo.vopros;
+	fotv1.textContent = objVo.otvet[0];
+	fotv2.textContent = objVo.otvet[1];
+	fotv3.textContent = objVo.otvet[2];
+	//intTek.textContent = "Допущено xx ошибок из ЧЧЧ";
+	//let countError = myClass.getErrorsInteger();
+	//let alls = myClass.getAllInteger()-1;
+	intTek.textContent = myClass.getMessageError("XfromN");
+	msgErrors.textContent = myClass.getMessageError("UHaveXErrors");
+	//intTek.textContent = `Допущено ошибок: ${countError}. Всего вопросов ${alls}`;
+}
+
+function dw(text) {
+	document.write(`  ${text} `);
+	//document.write(text2);
+}
+
+class Vo {
+	static voVsegoInteger;
+	static voTekInteger;	
+	static mapErrors;
+	static map;
+	vopros = "место для Вашего вопроса?";
+	otvet = new Array();
+	otvetTrue = 0;
+	errored = false;
+		constructor(v,ot1,ot2,ot3,pusto){
+			Vo.mapErrors = window.mapsError;
+			Vo.map = window.maps;
+			this.vopros = v;
+			if(pusto=="") {
+				let max = 3;
+				let rnd = Math.floor(Math.random() * max);
+				if (rnd == 0) {
+					//alert(`rnd = ${rnd}`)
+					this.otvet = new Array(ot1, ot2, ot3);
+					this.otvetTrue = rnd + 1;
+				}
+				if (rnd == 1) {
+					this.otvet = new Array(ot2, ot1, ot3);
+					this.otvetTrue = rnd + 1;
+				}
+				if (rnd == 2) {
+					this.otvet = new Array(ot3, ot2, ot1);
+					this.otvetTrue = rnd + 1;
+				}
+			}
+			if(pusto!="") {
+				this.otvet = new Array(ot1, ot2, ot3);
+				this.otvetTrue=pusto;
+			// for(let i=1; i<=3; i++){
+			// 	this.otvet[i] = (i=1)? ot1: (i=2)? ot2: ot3;
+			// }
+
+			}
+			if (Vo.voVsegoInteger == undefined) Vo.voVsegoInteger=0;
+			if (Vo.voTekInteger == undefined) Vo.voTekInteger=0;
+			Vo.voVsegoInteger++;
+
+		}
+		getCopyObj(){
+			//Не проверено. Может не работать?
+			return new Vo(vopros, otvet[0], otvet[1], otvet[2], otvetTrue)
+		}
+		solution(voti){
+				//if(vot=this.otvet[integer]) return true;
+				if(voti=this.otvetTrue) return true;
+				return false;
+		}
+		static getAllInteger(){
+			return Vo.map.size;
+		}
+		static getErrorsInteger(){
+			return Vo.mapErrors.size;
+		}
+		static getMessageError(variantMessage){
+			let countError = Vo.getErrorsInteger();
+			let alls = Vo.getAllInteger()-1;
+			let tested = Vo.voTekInteger+1;
+			let testedText = `На экране ${tested}`;
+			testedText = (tested>alls)? ` Вы ответили на ${alls} `: ` На экране ${tested} `;
+			//return `Допущено ошибок: ${countError}. ${testedText} вопрос из ${alls}`;
+			if (variantMessage=="XfromN"){
+				return `${testedText} вопрос из ${alls}`;
+			}
+			if (variantMessage=="UHaveXErrors"){
+				return `Допущено ошибок: ${countError}.`;
+			}
+
+		}
+
+		static getTek(){
+			//alert(`static = ${Vo.voVsegoInteger}`);
+			if (Vo.voTekInteger<Vo.voVsegoInteger){
+			let dd = window.maps.get(Vo.voTekInteger);
+			return dd;
+			}
+			//if (document.voTekInteger)
+		}
+		static next(){
+			//alert(`static = ${Vo.voVsegoInteger}`);
+			if (Vo.voTekInteger<Vo.voVsegoInteger-1){
+			//let dd1 = window.maps.size;
+			//let dd = window.maps.get(Vo.voTekInteger);
+			Vo.voTekInteger++;
+			//return dd;
+			}
+			//if (document.voTekInteger)
+		}
+		static creatDiv(){
+			dw(`<div name="voDiv" id="voDiv" align="center">`);
+
+//new Table(`<div name="voDiv" id="voDiv" align="center">`,2,3);
+dw(`<div name="voDiv" id="voDiv" align="center">`);
+dw(`	<span name="vopros" id="vopros"><h3> привет, ученик! начнем тест? </h3></span>`);
+dw(`	<br>`);
+dw(`	<br>`);
+dw(`	<button name="otv" id="otv1" onclick=proverka(1)>да</button>`);
+dw(`	<button name="otv" id="otv2" onclick=proverka(2)>да</button>`);
+dw(`	<button name="otv" id="otv3" onclick=proverka(3)>да</button>`);
+dw(`	<br>`);
+dw(`	<br>`);
+dw(`	<br>`);
+dw(`	<br>`);
+dw(`	<br>`);
+dw(`	<span name="intTek" id="intTek"><h3> что то</h3></span>`);
+dw(`	<br>`);
+dw(`	<br>`);
+dw(`	<span name="msgErrors" id="msgErrors"><h3> что то</h3></span>`);
+dw(`</div>`);
+
+dw(`	<br>`);
+dw(`	<br>`);
+
+dw(`<div name="podval" id="podval" align="center">`);
+dw(`	<br>`);
+dw(`	<br>`);
+//dw(`	<span name="itog" id="itog"><h3> Итог</h3></span>`);
+dw(`</div>`);
+		}
+}
+
+//class VoStart extends Vo{
+	//static voVsegoInteger;
+//}
+
+
+class Table {
+	table=null;
+	tbody=null;
+	columns=null;
+	tr=null;
+	
+	constructor (text, col, cols ){
+		this.columns = cols;
+		this.table = document.createElement('table');
+		this.tbody = document.createElement('tbody');
+		this.tr = document.createElement('tr');
+		let tdd;
+		for(let i=0; i<this.columns;i++){
+			tdd = document.createElement('td');
+			if(i==col-1) {tdd.innerHTML = text};
+			this.tr.appendChild(tdd);
+		}
+		this.tbody.appendChild(this.tr);
+		this.table.appendChild(this.tbody);
+		
+		let boddy = document.getElementById("body");
+		boddy.appendChild(this.table);
+	}
+	addrow(ch){
+		this.tbody.appendChild()
+	}	
+}
